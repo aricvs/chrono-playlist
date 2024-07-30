fe261c7605c64d972025e2d2bd5dee34f413e395
Ariel Santos <146785399+aricvs@users.noreply.github.com>
2024-07-30 18:41:30 -0300

    Clean up code and add comments

--- app.py
+++ app.py
@@ -7,22 +7,21 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 # Constants
-# TODO remove the need to put the redirect URI in the terminal
 CLIENT_ID = "7079456555d14329bfb98425a4f65306"
 CLIENT_SECRET = "366ab3afe1f8448f80d0c8176a111c56"
 SCOPE = "playlist-modify-private"
 REDIRECT_URI = "http://localhost:1234"
 
+# Scrape the Billboard website and get the song names in a list
 def get_songs(date, username, playlist_name):
     billboard_response = requests.get(
         f"https://www.billboard.com/charts/hot-100/{date}"
     )
     billboard_response.raise_for_status()
-
     soup = BeautifulSoup(billboard_response.text, "html.parser")
     return [name.get_text().strip() for name in soup.select("h3.c-title.a-no-trucate")]
     
-
+# Create a private Spotify playlist on the user's account
 def create_playlist(username, playlist_name, sp):
     playlist = sp.user_playlist_create(
         user=username,
@@ -32,19 +31,21 @@ def create_playlist(username, playlist_name, sp):
     )
     return playlist["id"]
 
+# Search and get the Spotify URI for each song and return a list with all the URIs
 def get_uris(song_names, sp, date):
     songs_uris = []
     for song_name in song_names:
         song_uri = sp.search(q=f"track: {song_name} year: {date.split('-')[0]}", limit=1)["tracks"]["items"][0]["uri"]
-        # TODO make this show up on the user facing GUI somehow
         print(f"Loading URI {song_uri.split(":")[2]}")
         songs_uris.append(song_uri.split(":")[2])
     return songs_uris
 
+# Route the main homepage for the webapp with the date and username form
 @app.route("/")
-def root():
+def home():
     return render_template("index.html")
 
+# Route the submit page getting data from the form and run all the functions
 @app.route("/submit", methods=["POST"])
 def submit():
     date = request.form["date"]
@@ -71,7 +72,8 @@ def submit():
         user=username, playlist_id=playlist_id, tracks=songs_uris
     )
     
-    return "Success"
+    # Add function to create another playlist
+    return "Success! Playlist created!"
 
 
 if __name__ == "__main__":
