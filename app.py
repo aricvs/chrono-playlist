c9c976cedd295f6ec85dff83fa6338e800d95559
Ariel Santos <146785399+aricvs@users.noreply.github.com>
2024-07-30 13:36:37 -0300

    Add base time machine code to process the data

--- app.py
+++ app.py
@@ -20,8 +20,50 @@ def root():
 
 @app.route("/submit", methods=["POST"])
 def submit():
-    print(request.form["date"])
-    print(request.form["username"])
+    date = request.form["date"]
+    username = request.form["username"]
+    playlist_name = f"{date} Billboard 100"
+
+    billboard_response = requests.get(
+        f"https://www.billboard.com/charts/hot-100/{date}"
+    )
+    billboard_response.raise_for_status()
+
+    soup = BeautifulSoup(billboard_response.text, "html.parser")
+    song_names = [
+        name.get_text().strip() for name in soup.select("h3.c-title.a-no-trucate")
+    ]
+
+    sp = spotipy.Spotify(
+        oauth_manager=SpotifyOAuth(
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            scope=SCOPE,
+            redirect_uri=REDIRECT_URI,
+            username=username,
+            show_dialog=True,
+            cache_path="token.txt",
+        )
+    )
+
+    playlist = sp.user_playlist_create(
+        user=username,
+        name=playlist_name,
+        public=False,
+        collaborative=False,
+    )
+    playlist_id = playlist["id"]
+
+    songs_uris = []
+    for song_name in song_names:
+        song_uri = sp.search(q=f"track: {song_name} year: {date.split('-')[0]}", limit=1)["tracks"]["items"][0]["uri"]
+        print(f"Loading URI {song_uri.split(":")[2]}")
+        songs_uris.append(song_uri.split(":")[2])
+
+    sp.user_playlist_add_tracks(
+        user=username, playlist_id=playlist_id, tracks=songs_uris
+    )
+    
     return "Success"
 
 
