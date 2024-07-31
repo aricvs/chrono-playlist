11c5bff026667aaee92c46856b1ce056331425a2
Ariel Santos <146785399+aricvs@users.noreply.github.com>
2024-07-31 11:29:37 -0300

    Fix f string formatting issue

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ CLIENT_SECRET = "366ab3afe1f8448f80d0c8176a111c56"
 SCOPE = "playlist-modify-private"
 REDIRECT_URI = "http://localhost:1234"
 
+
 # Scrape the Billboard website and get the song names in a list
 def get_songs(date, username, playlist_name):
     billboard_response = requests.get(
@@ -20,7 +21,8 @@ def get_songs(date, username, playlist_name):
     billboard_response.raise_for_status()
     soup = BeautifulSoup(billboard_response.text, "html.parser")
     return [name.get_text().strip() for name in soup.select("h3.c-title.a-no-trucate")]
-    
+
+
 # Create a private Spotify playlist on the user's account
 def create_playlist(username, playlist_name, sp):
     playlist = sp.user_playlist_create(
@@ -31,19 +33,24 @@ def create_playlist(username, playlist_name, sp):
     )
     return playlist["id"]
 
+
 # Search and get the Spotify URI for each song and return a list with all the URIs
 def get_uris(song_names, sp, date):
     songs_uris = []
     for song_name in song_names:
-        song_uri = sp.search(q=f"track: {song_name} year: {date.split('-')[0]}", limit=1)["tracks"]["items"][0]["uri"]
-        print(f"Loading URI {song_uri.split(":")[2]}")
+        song_uri = sp.search(
+            q=f"track: {song_name} year: {date.split('-')[0]}", limit=1
+        )["tracks"]["items"][0]["uri"]
+        print(f"Loading URI {song_uri.split(':')[2]}")
         songs_uris.append(song_uri.split(":")[2])
     return songs_uris
+
 
 # Route the main homepage for the webapp with the date and username form
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 # Route the submit page getting data from the form and run all the functions
 @app.route("/submit", methods=["POST"])
@@ -70,7 +77,7 @@ def submit():
     sp.user_playlist_add_tracks(
         user=username, playlist_id=playlist_id, tracks=songs_uris
     )
-    
+
     return render_template("submit.html")
 
 
